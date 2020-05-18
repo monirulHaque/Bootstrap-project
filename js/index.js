3c6807032cbe1e5fda0f1dfaa59e58a48f69d255
monirulHaque <monirul.haque@g.bracu.ac.bd>
2020-05-18 17:18:13 +0600

    End of the course

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
+    //tooltip script
+    // $('[data-toggle="tooltip"]').tooltip();
+    //carousel slide change interval
+    $("#mycarousel").carousel( { interval: 2000 } );
+    // $("#carousel-pause").click(function(){
+    //     $("#mycarousel").carousel('pause');
+    // });
+    // $("#carousel-play").click(function(){
+    //     $("#mycarousel").carousel('cycle');
+    // });
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+            $("#carouselButton").removeClass("btn-danger");
+            $("#carouselButton").addClass("btn-success");
+        }
+        else {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+            $("#carouselButton").removeClass("btn-success");
+            $("#carouselButton").addClass("btn-danger");
+        }
+    });
+
     $("#reservationButton").click(function(){
         $("#reservationModal").modal();
     });
@@ -6,5 +33,6 @@ $(document).ready(function(){
     $("#loginButton").click(function(){
         $("#loginModal").modal();
     });
+
 
   });
